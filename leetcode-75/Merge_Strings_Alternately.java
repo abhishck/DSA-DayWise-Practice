0facc145da08fdf7a04761852a56af8a4bfc4c74
Abhishek Rathore <abhishekrathore94500@gmail.com>
2026-02-02 22:58:02 +0530

    day-1

---
+++ leetcode-75/Merge_Strings_Alternately.java
@@ -0,0 +1,31 @@
+public class Merge_Strings_Alternately{
+    public static String mergeAlternately(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+        StringBuilder sb=new StringBuilder("");
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+        while(i<n){
+            sb.append(word1.charAt(i));
+            i++;
+        }
+        while(j<m){
+            sb.append(word2.charAt(j));
+            j++;
+        }
+        return sb.toString();
+
+    }
+    public static void main(String args[]){
+        String word1="abc";
+        String word2="pqr";
+       String result= mergeAlternately(word1, word2);
+       System.out.println(result);
+    }
+}
