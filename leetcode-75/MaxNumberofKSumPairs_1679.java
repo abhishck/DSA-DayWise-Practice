ff6c64126c775fa1a7a2de944b8fa731977a5969
Abhishek Rathore <abhishekrathore94500@gmail.com>
2026-02-10 00:27:17 +0530

    day-7 ques-2

---
+++ leetcode-75/MaxNumberofKSumPairs_1679.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class MaxNumberofKSumPairs_1679 {
+     public static int maxOperations(int[] nums, int k){
+        int n=nums.length;
+        Arrays.sort(nums);
+        int count=0;
+        int i=0;
+        int j=n-1;
+        while(i<j){
+         int value=nums[i]+nums[j];
+         if(value > k){
+            j--;
+         }else if(value < k){
+            i++;
+         }else{
+            count++;
+            i++;
+            j--;
+         }
+        }
+        return count;
+     }
+     public static void main(String[] args) {
+       int nums[]={1,2,3,4};
+       int k=5;
+       System.out.println(maxOperations(nums, k));
+     }
+}
