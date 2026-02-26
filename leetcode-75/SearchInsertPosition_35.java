421bb1e5a99d3ede26158fbfad37f98211605ea5
Abhishek Rathore <abhishekrathore94500@gmail.com>
2026-02-26 23:25:32 +0530

    search insert

---
+++ leetcode-75/SearchInsertPosition_35.java
@@ -0,0 +1,24 @@
+public class SearchInsertPosition_35{
+     public static int searchInsert(int[] nums, int target) {
+        int n=nums.length;
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return start ;
+    }
+    public static void main(String[] args) {
+        int nums[]={1,3,5,6};
+        int target=5;
+        System.out.println(searchInsert(nums,target));
+    }
+}
